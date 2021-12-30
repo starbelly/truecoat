b887a573e8d9dd4cb9517f01d040110e703a828b
Bryan Paxton <bryan@starbelly.io>
2021-12-30 11:52:04 -0600

    v0.4.761

--- src/truecoat.erl
+++ src/truecoat.erl
@@ -1,7 +1,19 @@
+%%%-------------------------------------------------------------------
+%% @doc `telemetry' allows you to invoke certain functions whenever a
+%% particular event is emitted.
+%%
+%% For more information see the documentation for {@link attach/4}, {@link attach_many/4}
+%% and {@link execute/2}.
+%% @end
+%%%-------------------------------------------------------------------
 -module(truecoat).
 
 -export([lot_count/0]).
 
+%% @doc Attaches the handler to many events.
+%%
+%% All the handlers are executed by the process dispatching event. If the function fails (raises,
+%% exits or throws) a handler failure event is emitted and then the handler is removed.
 lot_count() ->
     Str = ["Well, heck, if you wanna, if you wanna play games here!",
            " ",
